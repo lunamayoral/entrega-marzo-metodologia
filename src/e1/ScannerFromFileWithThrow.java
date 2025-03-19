713c617ef94882283f981f235f040e1c4cabba8e
Samug <samuel.garciabarrio@edu.uah.es>
2025-03-19 13:16:51 +0100

    ejemplo 3 Throw

--- src/e1/ScannerFromFileWithThrow.java
+++ src/e1/ScannerFromFileWithThrow.java
@@ -4,10 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class ScannerFromFileWithThrow {
-    public static void main(String[] args) throws FileNotFoundException {
-        // to be handled by next higher-level method
+    public static void main(String[] args) {
+        try {
+            readFile();
+        } catch (FileNotFoundException ex) {
+            System.err.println("Error: Archivo no encontrado. Revisa la ruta y vuelve a intentar.");
+            ex.printStackTrace();
+        }
+    }
+
+    public static void readFile() throws FileNotFoundException {
         Scanner in = new Scanner(new File("test.in"));
-        // this method may throw FileNotFoundException
-        // main logic here ...
+        while (in.hasNextLine()) {
+            System.out.println(in.nextLine());
+        }
     }
 }
