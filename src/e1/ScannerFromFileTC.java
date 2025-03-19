8c55d8a18af205ef1b26855f71588a685066b4b9
Usuario <irene.martingarcia@edu.uah>
2025-03-19 12:44:02 +0100

    ejemplo1

---
+++ src/e1/ScannerFromFileTC.java
@@ -0,0 +1,14 @@
+package e1;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ScannerFromFileTC {
+    public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(new File("test.in"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado: " + e.getMessage());
+        }
+    }
+}
