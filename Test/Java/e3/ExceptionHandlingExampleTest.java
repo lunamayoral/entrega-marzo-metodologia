f23cc652760825f4d51bf0c496d12bc8c8b57288
irenemartingarcia <irene.martingarcia@edu.uah.es>
2025-03-26 13:42:20 +0100

    ej7modificacion2

---
+++ Test/Java/e3/ExceptionHandlingExampleTest.java
@@ -0,0 +1,25 @@
+package e3;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ExceptionHandlingExampleTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void methodA() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void methodB() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void methodC() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void methodD() {
+    }
+}
