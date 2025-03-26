2f2bed1f95b7218f923605f7ea1e9a4342efcf5c
lunamayoral <lunabookblog@gmail.com>
2025-03-26 13:01:50 +0100

    test ej 2

---
+++ Test/Java/e2/MethodCallStackDemoTest.java
@@ -0,0 +1,30 @@
+package e2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MethodCallStackDemoTest {
+
+    @Test
+    void main() {
+        assertDoesNotThrow(() -> MethodCallStackDemo.main(new String[]{}));
+    }
+
+    @Test
+    void methodA() {
+        assertDoesNotThrow(() -> MethodCallStackDemo.methodA());
+
+
+    }
+
+    @Test
+    void methodB() {
+        assertDoesNotThrow(() -> MethodCallStackDemo.methodB());
+    }
+
+    @Test
+    void methodC() {
+        assertDoesNotThrow(() -> MethodCallStackDemo.methodC());
+    }
+}
