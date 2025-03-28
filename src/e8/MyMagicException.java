97a0494531bba23db6435abcbd537b3d087dd8b4
DaniOrtegaR13 <d.ortegar@edu.uah.es>
2025-03-28 11:49:30 +0100

    Ejercio 8 Exception y MyMagicExceptions

---
+++ src/e8/MyMagicException.java
@@ -0,0 +1,10 @@
+package e8;
+
+public class MyMagicException extends Exception {
+    public MyMagicException(String message) {  // Constructor
+        super(message);
+    }
+}
+
+// Clase de prueba principal
+
---
+++ src/e8/MyMagicExceptionTest.java
@@ -0,0 +1,23 @@
+package e8;
+
+
+
+    public class MyMagicExceptionTest {
+
+        public static void magic(int number) throws MyMagicException {
+            if (number == 8) {
+                throw (new MyMagicException("you hit the magic number"));
+            }
+            System.out.println("hello");  // Se omite si se lanza la excepción
+        }
+
+        public static void main(String[] args) {
+            try {
+                magic(9);   // No lanza excepción
+                magic(8);   // Lanza excepción
+            } catch (MyMagicException ex) {   // Manejador de excepciones
+                ex.printStackTrace();
+            }
+        }
+    }
+
