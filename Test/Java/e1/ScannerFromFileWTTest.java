aff3e698036360ccc68a9a3ac27c7e6abe57aee4
lunamayoral <lunabookblog@gmail.com>
2025-03-31 22:38:11 +0200

    test del ej 1

--- Test/Java/e1/ScannerFromFileWTTest.java
+++ Test/Java/e1/ScannerFromFileWTTest.java
@@ -10,15 +10,23 @@ class ScannerFromFileWTTest {
 
     @Test
     void main() {
-        try {
-            ScannerFromFileWT.main(new String[]{});
-        } catch (Exception e) {
-            assertEquals(true, false, "Main method threw an exception: " + e.getMessage());
-        }
+        assertDoesNotThrow(() -> ScannerFromFileWT.main(new String[]{}));
     }
 
     @Test
     void readFile() {
+        File testFile0 = new File("test.in");
+        assertDoesNotThrow(() -> ScannerFromFileWT.readFile());
 
+        File testFile = new File("test.in");
+        try (FileWriter writer = new FileWriter(testFile)) {
+            writer.write("Primera línea\nSegunda línea\nTercera línea");
+        } catch (IOException e) {
+            throw new RuntimeException("Error al crear el archivo de prueba.", e);
+        }
+        assertDoesNotThrow(() -> ScannerFromFileWT.readFile());
+        assertTrue(testFile.exists());
+        testFile.delete();
     }
+
 }
